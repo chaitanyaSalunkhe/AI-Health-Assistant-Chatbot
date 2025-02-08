14a6e898b2a551e555043b0e68da0e7194d5dffd
chaitanyaSalunkhe <160728267+chaitanyaSalunkhe@users.noreply.github.com>
2025-02-08 09:56:16 +0530

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-from geopy.geocoders import Nominatim
+import geopy.geocoders import Nominatim
 import streamlit as st
 import nltk
 from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
